a8070a821c279c8382400acea0d4abcea725c0c7
Timothy Brown <tabrownies@gmail.com>
2020-10-07 22:56:16 -0600

    Can now get the values and the values are sorted. All that remains is formating and cosmetics.

--- script.js
+++ script.js
@@ -1,42 +1,86 @@
-document.getElementById("weatherSubmit").addEventListener("click", (event) => {
+/*document.getElementById("weatherSubmit").addEventListener("click", (event) => {
     event.preventDefault();
     const value = document.getElementById("weatherInput").value;
     if (value === "")
         return;
-    console.log(value);
-    const url = "http://api.openweathermap.org/data/2.5/weather?q=" + value + ",US&units=imperial" + "&APPID=6ac1005487fd66a76c6b028559aeaf9c";
+    console.log(createFiveDayForcastTree(document.getElementById("weatherInput").value));
+    
+});*/
+function updateCurrentWeatherDiv(currentWeather){
+    let result = '<ul>';
+    for(item in currentWeather.main){
+        result += `<li>${item}:  ${currentWeather.main[item]}</li>`;
+    }
+    for(item in currentWeather.weather[0]){
+        result += `<li>${item}:  ${currentWeather.weather[0][item]}</li>`;
+    }
+    result += `</ul>`;
+    document.getElementById("currentWeather").innerHTML = result;
+
+}
+function getCurrentWeather(place){
+    let currentWeather;
+    const url = "http://api.openweathermap.org/data/2.5/weather?q=" + place + ",US&units=imperial" + "&APPID=6ac1005487fd66a76c6b028559aeaf9c";
     fetch(url)
         .then(function (response) {
             return response.json();
         }).then(function (json) {
-            let results = "";
-            results += '<h2>Weather in ' + json.name + "</h2>";
-            for (let i = 0; i < json.weather.length; i++) {
-                results += '<img src="http://openweathermap.org/img/w/' + json.weather[i].icon + '.png"/>';
-            }
-            results += '<h2>' + json.main.temp + " &deg;F</h2>"
-            results += "<p>"
-            for (let i = 0; i < json.weather.length; i++) {
-                results += json.weather[i].description
-                if (i !== json.weather.length - 1)
-                    results += ", "
-            }
-            results += "</p>";
-            document.getElementById("weatherResults").innerHTML = results;
-        }).catch((error) => {
-            document.getElementById("weatherResults").innerHTML = `No city, ${value}, found`;
+            currentWeather = json;
+            console.log(currentWeather);
+            updateCurrentWeatherDiv(currentWeather);
         });
-    const url2 = "http://api.openweathermap.org/data/2.5/forecast?q=" + value + ", US&units=imperial" + "&APPID=6ac1005487fd66a76c6b028559aeaf9c";
-    fetch(url2)
+    
+}
+function updateFiveDayForcastDiv(fiveDayForcast){
+    let result = `<ul>`;
+    console.log(fiveDayForcast.length);
+    for(item in fiveDayForcast){
+        result += `<li class="five-day-forcast-day"><div>`;
+        for(value in fiveDayForcast[item]){
+            result += ` ${value} `;
+        }
+        result+= `</div></li>`;
+    }
+    result += `</ul>`;
+    console.log(result);
+    document.getElementById("fiveDayForcast").innerHTML = result;
+}
+function getFiveDayForcast(place) {
+    function Day(date) {
+        this.date = date;
+        this.times = [];
+    };
+    
+    function Time(list) {
+        this.info = list;
+    }
+    let fiveDayForcast = [];
+    const url = "http://api.openweathermap.org/data/2.5/forecast?q=" + place + ", US&units=imperial" + "&APPID=6ac1005487fd66a76c6b028559aeaf9c";
+    fetch(url)
         .then(function (response) {
             return response.json();
         }).then(function (json) {
-            let forecast = "";
-            for (let i = 0; i < json.list.length; i++) {
-                forecast += "<h2>" + moment(json.list[i].dt_txt).format('MMMM Do YYYY, h:mm:ss a') + "</h2>";
-                forecast += "<p>Temperature: " + json.list[i].main.temp + "</p>";
-                forecast += '<img src="http://openweathermap.org/img/w/' + json.list[i].weather[0].icon + '.png"/>'
+            for (let i = 0; i < json.list.length; ++i) {
+                let currentDay = moment(json.list[i].dt_txt).format('MMMM Do');
+                let inArray = false;
+                let dayIndex = 0;
+                for (j in fiveDayForcast) {
+                    if (fiveDayForcast[j].date === currentDay) {
+                        inArray = true;
+                        dayIndex = j;
+                    }
+                }
+                if (!inArray) {
+                    fiveDayForcast.push(new Day(currentDay));
+                    dayIndex = fiveDayForcast.length - 1;
+                }
+                fiveDayForcast[dayIndex].times.push(json.list[i]);
             }
-            document.getElementById("forecastResults").innerHTML = forecast;
+            updateFiveDayForcastDiv(fiveDayForcast);
         });
-});
+}
+getCurrentWeather("Provo");
+getFiveDayForcast("Provo");
+
+
+
